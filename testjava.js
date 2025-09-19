985eadc411c8d51820a6a251a4092e0722a63f84
Rafeyfa <rfeyfaasl@gmail.com>
2025-09-19 19:24:07 +0700

    first commit

---
+++ testjava.js
@@ -0,0 +1,11 @@
+// Minta input dari user
+let nama = prompt("Siapa namamu?");
+
+// Buat pesan
+let pesan = "Selamat datang,hhhhhhhhhhh " + nama + "! Semoga harimu menyenangkan 😊";
+
+// Tampilkan ke console
+console.log(pesan);
+
+// Tampilkan juga dengan alert
+alert(pesan);
